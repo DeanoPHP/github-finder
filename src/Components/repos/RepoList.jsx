16ab00888203a40a0e59b0deb14c281fa8f9851d
DeanoLark <deanolark@gmail.com>
2024-01-18 16:18:07 +0000

    RepoItems style, Refactor from GitContext to GithubActions

--- src/Components/repos/RepoList.jsx
+++ src/Components/repos/RepoList.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "react"
+import RepoItem from "./RepoItem"
 
-function RepoList({repos}) {
+function RepoList({ repos }) {
   return (
     <div className="rounded-lg shadow-lg card bg-base-100">
       <div className="card-body">
@@ -8,7 +9,7 @@ function RepoList({repos}) {
             Latest Repositories
         </h2>
         {repos.map((repo) => (
-            <h3>{repo.name}</h3>
+            <RepoItem key={repo.id} repo={repo} />
         ))}
       </div>
     </div>
@@ -16,8 +17,8 @@ function RepoList({repos}) {
     
 }
 
-RepoList.propTypes = {
-    repos: PropTypes.array,
-}
+// RepoList.propTypes = {
+//     repos: PropTypes.array.isRequired,
+// }
 
 export default RepoList
